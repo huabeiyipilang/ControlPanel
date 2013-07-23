9988c6f79192e90135e2abc6808db2fe41f401c8
Carl Li <huabeiyipilang@gmail.com>
2013-07-23 17:14:33 +0800

    tips基本功能实现

--- src/cn/kli/controlpanel/guide/TipsView.java
+++ src/cn/kli/controlpanel/guide/TipsView.java
@@ -1,11 +1,17 @@
 package cn.kli.controlpanel.guide;
 
 import cn.kli.controlpanel.R;
+import cn.kli.utils.klilog;
 import android.content.Context;
 import android.os.Handler;
 import android.os.Message;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.Animation.AnimationListener;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -14,6 +20,9 @@ public class TipsView extends LinearLayout {
 	private String[] mStrings = null;
 	private int index = 0;
 	private TextView mDisplay;
+	private String mCurrentTips;
+	private Animation mSlideInAnim;
+	private Animation mSlideOutAnim;
 	private Handler mHandler = new Handler(){
 
 		@Override
@@ -22,44 +31,98 @@ public class TipsView extends LinearLayout {
 			switch(msg.what){
 			case MSG_NEXT:
 				if(mStrings != null && mStrings.length > 0){
-					if(index > mStrings.length){
+					if(index >= mStrings.length){
 						index = 0;
 					}
-					mDisplay.setText(mStrings[index++]);
-					sendEmptyMessageDelayed(MSG_NEXT, 5000);
+					mCurrentTips = mStrings[index++];
+					klilog.i("show tips:"+mCurrentTips);
+					changeTipsWithAnim();
+					sendEmptyMessageDelayed(MSG_NEXT, 10000);
 				}
 				break;
 			}
 		}
 		
 	};
+	
+	private void changeTipsWithAnim(){
+		mDisplay.startAnimation(mSlideOutAnim);
+	}
 
 	public TipsView(Context context) {
 		super(context);
-		
+		init();
 	}
 
 	public TipsView(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		
+		init();
 	}
-
-	public void start(){
+	
+	private void init(){
+		LayoutInflater inflater = LayoutInflater.from(getContext());
+		inflater.inflate(R.layout.tips_view, this);
+		mDisplay = (TextView)findViewById(R.id.tv_tips_show);
 		mStrings = getContext().getResources().getStringArray(R.array.guide_tips);
-		mDisplay = new TextView(getContext()); 
-		mDisplay.setHeight(ViewGroup.LayoutParams.MATCH_PARENT);
-		mDisplay.setWidth(ViewGroup.LayoutParams.MATCH_PARENT);
+		mSlideInAnim = AnimationUtils.loadAnimation(getContext(), R.anim.slide_in_up);
+		mSlideInAnim.setAnimationListener(new AnimationListener(){
+
+			@Override
+			public void onAnimationEnd(Animation arg0) {
+				klilog.i("Animation end");
+			}
+
+			@Override
+			public void onAnimationRepeat(Animation arg0) {
+				
+			}
+
+			@Override
+			public void onAnimationStart(Animation arg0) {
+				klilog.i("Animation start");
+			}
+			
+		});
+		
+		mSlideOutAnim = AnimationUtils.loadAnimation(getContext(), R.anim.slide_out_down);
+		mSlideOutAnim.setAnimationListener(new AnimationListener(){
+
+			@Override
+			public void onAnimationEnd(Animation arg0) {
+				mDisplay.setText(mCurrentTips);
+				mDisplay.startAnimation(mSlideInAnim);
+			}
+
+			@Override
+			public void onAnimationRepeat(Animation arg0) {
+				
+			}
+
+			@Override
+			public void onAnimationStart(Animation arg0) {
+			}
+			
+		});
+	}
+	
+	public void start(){
 		mHandler.sendEmptyMessage(MSG_NEXT);
+	}
+	
+	public void stop(){
+		mHandler.removeMessages(MSG_NEXT);
 	}
 
 	@Override
 	protected void onAttachedToWindow() {
 		super.onAttachedToWindow();
+		start();
 	}
 
 	@Override
 	protected void onDetachedFromWindow() {
 		super.onDetachedFromWindow();
+		stop();
 	}
 	
 	
--- src/cn/kli/controlpanel/launcher/GroupSettingsFragment.java
+++ src/cn/kli/controlpanel/launcher/GroupSettingsFragment.java
@@ -1,11 +1,5 @@
 package cn.kli.controlpanel.launcher;
 
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
 public class GroupSettingsFragment extends GroupBaseFragment {
 
 }
